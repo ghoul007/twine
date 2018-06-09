d0f18b32183c33a9361c0a079525a48e29d811ba
ahmed <ghoul.ahmed5@gmail.com>
2018-06-09 01:32:08 +0100

    test configstore

---
+++ test/credential-manager.js
@@ -0,0 +1,21 @@
+const chai = require('chai')
+const expect = chai.expect
+const sinon = require('sinon')
+const inquirer = require('inquirer')
+const CredentialManager = require('../lib/credential-manager')
+
+
+describe('a crdential manager', () => {
+    var creds;
+    beforeEach(() => {
+        creds = new CredentialManager('twine-test')
+    });
+
+    it('should popmt the user', async () => {
+        sinon.stub(inquirer, 'prompt').resolves({ key: 'foo', secret: "bar" })
+        let [key, secret] = await creds.getKeyAndSecret();
+        expect(key).to.equal('foo')
+        expect(secret).to.equal('bar')
+        inquirer.prompt.restore()
+    });
+});
