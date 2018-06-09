58d6cb272eac0a79eb590595eb28045619f33c93
ahmed <ghoul.ahmed5@gmail.com>
2018-06-09 01:31:07 +0100

    configstore +inquirer to store key and scret

--- bin/twine.js
+++ bin/twine.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
-console.log('test ahmed !!')
+const CredentialManager = require('../lib/credential-manager');
+
+async function main(){
+
+    const creds = new CredentialManager('twine');
+    let [key, secret] = await creds.getKeyAndSecret();
+    
+    console.log(key, secret);
+}
+
+
+main().catch( console.error)
